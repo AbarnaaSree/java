c24209dccc9820d86fb26d1fcfaa6eecadb47926
Abarnaa Sree <abarnaasree23@gmail.com>
2026-08-16 22:41:29 +0530

    k

---
+++ graph.java
@@ -0,0 +1,23 @@
+public class graph {
+    static int rows,cols;
+    static int[][] grid;
+    statii void dfs(int rows,int cols){
+        if(r<0 || r>=rows || c<0 || c>=cols){
+            retunr;
+        }
+        if(grid[r][c]==0){
+            return'
+        }
+            grid[r][c]=0;
+            dfs(r-1,c);
+            dfs(r+1,c);
+            dfs(r,c-1);
+            dfs(r,c+1);
+    }
+    public static void main(String[] args){
+       Scanner sc-new Scanner(System.for (int rows=sc.nextInt) = 0; rows=sc.nextInt) < lst.size(); rows=sc.nextInt)++) {
+           Object elem = lst.get(rows=sc.nextInt));
+           
+       }
+    }
+}
